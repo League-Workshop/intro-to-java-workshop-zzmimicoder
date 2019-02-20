ebb3c285d1fbe6d051d46f9c0a03507872310f6b
KingArman999 <44267696+KingArman999@users.noreply.github.com>
2019-02-20 14:27:47 -0800

    Day 3
    DAY 3!!!!!!!!!!! GLJFOVIAWRUGNPIEAJTYPI

--- src/section3/SpeakAndSpell.java
+++ src/section3/SpeakAndSpell.java
@@ -6,18 +6,28 @@ import com.sun.speech.freetts.Voice;
 import com.sun.speech.freetts.VoiceManager;
 
 public class SpeakAndSpell {
-
 	public static void main(String[] args) {
-		// 1. Use the speak method to say the word. "e.g. spell mandlebrot"
-
-		// 2. Catch the user's answer in a String
-
-		// 3. If the user spelled the word correctly, speak "correct"
-
-		// 4. Otherwise say "wrong"
-
-		// 5. repeat the process for other words
-		
+		speak("Pneumonoultramicroscopicsilicovolcanoconiosis");
+		String ans = JOptionPane.showInputDialog("What Do You Think This Word Is?\n(Do Not Capitalize The First Letter)");
+		if (ans == "pneumonoultramicroscopicsilicovolcanoconiosis") {
+			speak("Correct");
+		} else {
+			speak("Wrong");
+		}
+		speak("Antidisestablishmentarianism");
+		ans = JOptionPane.showInputDialog("What Do You Think This Word Is?\n(Do Not Capitalize The First Letter)");
+		if (ans == "Antidisestablishmentarianism") {
+			speak("Correct");
+		} else {
+			speak("Wrong");
+		}
+		speak("Methionylthreonylthreonyglutaminylarginylisoleucine");
+		ans = JOptionPane.showInputDialog("What Do You Think This Word Is?\n(Do Not Capitalize The First Letter)");
+		if (ans == "methionylthreonylthreonyglutaminylarginylisoleucine") {
+			speak("Correct");
+		} else {
+			speak("Wrong");
+		}
 	}
 
 	static void speak(String words) {
